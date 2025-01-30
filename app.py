d7a2a8ee700fac63f03bf07a45f2ea2da8583067
BayuKuy <bayu20423004@digitechuniversity.ac.id>
2025-01-30 13:01:01 +0700

    Menambahkan Procfile dan mengedit app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import csv
+import os
 from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
@@ -34,4 +35,5 @@ def join():
     return render_template('join.html')  # Menampilkan halaman Join Us
 
 if __name__ == '__main__':
-    app.run(debug=True)
+     port = int(os.environ.get("PORT", 5000))
+     app.run(host="0.0.0.0", port=port)
